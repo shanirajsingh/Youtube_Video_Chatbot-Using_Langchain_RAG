aa34a3f29972b48d519a604bc4887a8e3aa238c5
shanirajsingh <125183224+shanirajsingh@users.noreply.github.com>
2026-05-28 13:50:14 +0530

    Refactor import statements for document chains

--- app.py
+++ app.py
@@ -27,13 +27,9 @@ from langchain_community.vectorstores import (
 from langchain_groq import ChatGroq
 
 # IMPORTANT FIX
-from langchain.chains.combine_documents import (
-    create_stuff_documents_chain
-)
+from langchain.chains.combine_documents import create_stuff_documents_chain
 
-from langchain.chains.combine_documents import (
-    create_retrieval_chain
-)
+from langchain.chains import create_retrieval_chain
 
 from langchain_core.prompts import (
     ChatPromptTemplate
